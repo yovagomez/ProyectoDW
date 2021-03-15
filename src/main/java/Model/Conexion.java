c7153986a0579d2ad001bef05d7831e1e17d9930
yovagomez <yovagomez@192.168.1.18>
2021-03-14 19:00:57 -0600

    Conexion con la Base de Datos

---
+++ src/main/java/Model/Conexion.java
@@ -0,0 +1,42 @@
+package Model;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author yovagomez
+ */
+public class Conexion {
+    
+    private static Conexion conexion;
+    private static final String DBURL = "jdbc:mysql://localhost:3306/proyecto?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    private static Connection conex = null;
+    
+    private Conexion(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            conex=DriverManager.getConnection(DBURL, "proyecto", "proyecto123_");
+            
+        } catch (ClassNotFoundException | SQLException
+                | NoSuchMethodException | SecurityException | InstantiationException
+                | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException e) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE,null,e);
+            
+        }
+    }
+    
+    public static synchronized Connection getConexion(){
+        if(conexion==null){
+            conexion=new Conexion();
+        }
+        
+        return conex;
+    }
+    
+}
