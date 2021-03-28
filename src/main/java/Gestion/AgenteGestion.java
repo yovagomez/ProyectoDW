2be8dae5b0e746de0660091dcce997fc1baaf49f
Daniel <Daniel@PC-Bretes>
2021-03-27 20:15:28 -0600

    Se completo las clases de Gestion-Agente y Gestion-Usuario.

--- src/main/java/Gestion/AgenteGestion.java
+++ src/main/java/Gestion/AgenteGestion.java
@@ -14,15 +14,15 @@ import Model.Agente;
  */
 public class AgenteGestion {
     
-    //Variables encargadas de llamar la Base Datos
+    //Variables encargadas de llamar la Base de Datos Agente
     private static final String SQL_GETAGENTES="SELECT * FROM agente";
     private static final String SQL_GETAGENTE="SELECT * FROM agente where id=? and idAgente=?";
-    private static final String SQL_INSERTAGENTES="insert into agente(idAgente, nombre, apellido1, apellido2) VALUES (?,?,?,?)";
+    private static final String SQL_INSERTAGENTES="INSERT into agente(idAgente, nombre, apellido1, apellido2) VALUES (?,?,?,?)";
     private static final String SQL_UPDATEAGENTES="UPDATE agente set nombre=?, apellido1=?, apellido2=? where id=? and idAgente=?";
     private static final String SQL_DELETEAGENTES="DELETE FROM agente where id=? and idAgente=?";
     
-    //Metodo encargado de llamar a todos los usuarios
-    public static ArrayList<Agente> getAgente() {
+    //Metodo encargado de llamar a todos los Agentes
+    public static ArrayList<Agente> getAgentes() {
         ArrayList<Agente> lista = new ArrayList<>();
         try {
             PreparedStatement sentencia = Conexion.getConexion()
@@ -41,5 +41,79 @@ public class AgenteGestion {
                     .log(Level.SEVERE, null, ex);
         }
         return lista;
-    }//Fin metodo de llamar todos los usuarios
+    }//Fin metodo de llamar todos los Agentes
+    
+    //Metodo encargado de llamar a un Agente
+    public static Agente getAgente(int id, String idAgente) {
+        Agente agente = null;
+        try {
+            PreparedStatement sentencia = Conexion.getConexion()
+                    .prepareStatement(SQL_GETAGENTE);
+            sentencia.setInt(1, id);
+            sentencia.setString(2, idAgente);
+            ResultSet rs = sentencia.executeQuery();
+            while (rs != null && rs.next()) {
+                agente = new Agente(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AgenteGestion.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return agente;
+    }//Fin metodo llamar un Agente
+    
+    //Metodo encargado de realizar un INSERT en Agente
+    public static boolean insertEstudiante(Agente agente) {
+        try {
+            PreparedStatement sentencia = Conexion.getConexion()
+                    .prepareStatement(SQL_INSERTAGENTES);
+            sentencia.setString(1, agente.getIdAgente());
+            sentencia.setString(2, agente.getNombre());
+            sentencia.setString(3, agente.getApellido1());
+            sentencia.setString(4, agente.getApellido2());
+            return sentencia.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(AgenteGestion.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }//Fin metodo encargado de el INSERT
+    
+    //Metodo encargado de realizar un Update en Agente
+    public static boolean updateEstudiante(Agente agente) {
+        try {
+            PreparedStatement sentencia = Conexion.getConexion()
+                    .prepareStatement(SQL_UPDATEAGENTES);
+            sentencia.setString(1, agente.getNombre());
+            sentencia.setString(2, agente.getApellido1());
+            sentencia.setString(3, agente.getApellido2());
+            sentencia.setInt(4, agente.getId());
+            sentencia.setString(5, agente.getIdAgente());
+            return sentencia.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(AgenteGestion.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }//Fin metodo encargado de el Update
+    
+    //Metodo encargado de realizar un DELETE en Agente
+    public static boolean deleteEstudiante(Agente agente) {
+        try {
+            PreparedStatement sentencia = Conexion.getConexion()
+                    .prepareStatement(SQL_DELETEAGENTES);
+            sentencia.setInt(1, agente.getId());
+            sentencia.setString(2, agente.getIdAgente());
+            return sentencia.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(AgenteGestion.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }//Fin metodo encargado de el DELETE
 }//Fin clase
--- src/main/java/Gestion/UsuarioGestion.java
+++ src/main/java/Gestion/UsuarioGestion.java
@@ -1,4 +1,12 @@
 package Gestion;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import Model.Usuario;
+import Model.Conexion;
 
 /**
  *
@@ -6,4 +14,118 @@ package Gestion;
  */
 public class UsuarioGestion {
     
+    //Variables encargadas de llamar a la Base de Datos Usuario
+    private static final String SQL_GETUSUARIOS="SELECT * FROM usuario";
+    private static final String SQL_GETUSUARIO="SELECT * FROM usuario where id=? and idUsuario=?";
+    private static final String SQL_INSERTUSUARIOS="INSERT into usuario(idUsuario, numCompra, nombre, apellido1, apellido2, correo, clave) VALUES (?,?,?,?,?,?,?)";
+    private static final String SQL_UPDATEUSUARIOS="UPDATE usuario set numCompra=?, nombre=?, apellido1=?, apellido2=?, correo=?, clave=? where id=? and idUsuario=?";
+    private static final String SQL_DELETEUSUARIOS="DELETE FROM usuario where id=? and idUsuario=?";
+    
+    //Metodo encargado de llamar a todos los Usuarios
+    public static ArrayList<Usuario> getUsuarios() {
+        ArrayList<Usuario> lista = new ArrayList<>();
+        try {
+            PreparedStatement sentencia = Conexion.getConexion()
+                    .prepareStatement(SQL_GETUSUARIOS);
+            ResultSet rs = sentencia.executeQuery();
+            while (rs != null && rs.next()) {
+                lista.add(new Usuario(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getInt(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getString(7),
+                        rs.getString(8)));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioGestion.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return lista;
+    }//Fin metodo de llamar todos los Usuarios
+    
+    //Metodo encargado de llamar a un Usuario
+    public static Usuario getUsuario(int id, String idUsuario) {
+        Usuario usuario = null;
+        try {
+            PreparedStatement sentencia = Conexion.getConexion()
+                    .prepareStatement(SQL_GETUSUARIO);
+            sentencia.setInt(1, id);
+            sentencia.setString(2, idUsuario);
+            ResultSet rs = sentencia.executeQuery();
+            while (rs != null && rs.next()) {
+                usuario = new Usuario(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getInt(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getString(7),
+                        rs.getString(8));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioGestion.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return usuario;
+    }//Fin metodo llamar un Usuario
+    
+    //Metodo encargado de realizar un INSERT en Usuario
+    public static boolean insertUsuario(Usuario usuario) {
+        try {
+            PreparedStatement sentencia = Conexion.getConexion()
+                    .prepareStatement(SQL_INSERTUSUARIOS);
+            sentencia.setString(1, usuario.getIdUsuario());
+            sentencia.setInt(2, usuario.getNumCompra());
+            sentencia.setString(3, usuario.getNombre());
+            sentencia.setString(4, usuario.getApellido1());
+            sentencia.setString(5, usuario.getApellido2());
+            sentencia.setString(6, usuario.getCorreo());
+            sentencia.setString(7, usuario.getClave());
+            return sentencia.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioGestion.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }//Fin metodo encargado de el INSERT
+    
+    //Metodo encargado de realizar un Update en Usuario
+    public static boolean updateEstudiante(Usuario usuario) {
+        try {
+            PreparedStatement sentencia = Conexion.getConexion()
+                    .prepareStatement(SQL_UPDATEUSUARIOS);
+            sentencia.setInt(1, usuario.getNumCompra());
+            sentencia.setString(2, usuario.getNombre());
+            sentencia.setString(3, usuario.getApellido1());
+            sentencia.setString(4, usuario.getApellido2());
+            sentencia.setString(5, usuario.getCorreo());
+            sentencia.setString(6, usuario.getClave());
+            sentencia.setInt(7, usuario.getId());
+            sentencia.setString(8, usuario.getIdUsuario());
+            return sentencia.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioGestion.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }//Fin metodo encargado de el Update
+    
+    //Metodo encargado de realizar un DELETE en Usuario
+    public static boolean deleteEstudiante(Usuario usuario) {
+        try {
+            PreparedStatement sentencia = Conexion.getConexion()
+                    .prepareStatement(SQL_DELETEUSUARIOS);
+            sentencia.setInt(1, usuario.getId());
+            sentencia.setString(2, usuario.getIdUsuario());
+            return sentencia.executeUpdate() > 0;
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioGestion.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }//Fin metodo encargado de el DELETE
 }//Fin clase
